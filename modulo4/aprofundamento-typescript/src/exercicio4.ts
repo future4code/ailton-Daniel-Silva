5c10abae1f352336edae0b237373842fe5843b73
Daniel Melo de Andrade Silva <daniel.mandrade77@gmail.com>
2022-08-11 19:34:47 -0300

    typescript-ex

---
+++ modulo4/aprofundamento-typescript/src/exercicio4.ts
@@ -0,0 +1,30 @@
+// type pokemon = {
+// 	name: string,
+//   types: string,
+// 	healthPoints: number
+// }
+
+// const pokemon1: pokemon = {
+//   name: "Charmander",
+//   types: "Fire",
+//   healthPoints: 28
+// }
+
+// const pokemon2: pokemon = {
+//   name: "Bulbasaur",
+//   types: "Grass/Poison",
+//   healthPoints: 31
+// }
+
+// const pokemon3: pokemon = {
+//   name: "Squirtle",
+//   types: "Water",
+//   healthPoints: 35
+// }
+
+// b) Utilizando o tsc exercicio4.ts
+
+// c) É necessário ir no package.json e criar o script "exercicio4": "tsc && node ./build/exercicio4.js" e rodar 
+// npm run exercicio4
+
+// d) tsc exercicio5.ts exercicio6.ts exercicio7.ts
