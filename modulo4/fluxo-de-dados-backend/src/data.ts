b8066cf047a50f7f3da78528862289c135f1cac2
Daniel Melo de Andrade Silva <daniel.mandrade77@gmail.com>
2022-08-17 17:31:48 -0300

    fluxo de dados backend

---
+++ modulo4/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,12 @@
+export type Product = {
+    id: string,
+    name: string,
+    price: number
+}
+
+export let arrayProducts: Product[] = [
+    {id: "1", name: "Camisa", price: 80},
+    {id: "2", name: "Short", price: 90},
+    {id: "3", name: "Calça", price: 120},
+    {id: "4", name: "Bermuda", price: 100}
+]
