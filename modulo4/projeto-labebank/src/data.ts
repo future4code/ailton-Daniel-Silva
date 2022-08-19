0e43cb191a081dc7c0268606e7516a157f8ccdc9
Daniel Melo de Andrade Silva <daniel.mandrade77@gmail.com>
2022-08-19 16:42:09 -0300

    labebank

---
+++ modulo4/projeto-labebank/src/data.ts
@@ -0,0 +1,12 @@
+export type User = {
+    name: string,
+    cpf: string,
+    birthDate: string
+}
+
+export const bankUsers: User[] = [
+    {name: "Leila", cpf: "14567812356", birthDate: "12/05/1999"},
+    {name: "Marcella", cpf: "13589423412", birthDate: "25/09/1991"},
+    {name: "Taynã", cpf: "78390023411", birthDate: "15/12/1992"},
+    {name: "Mariana", cpf: "90045678923", birthDate: "20/12/1992"}
+]
