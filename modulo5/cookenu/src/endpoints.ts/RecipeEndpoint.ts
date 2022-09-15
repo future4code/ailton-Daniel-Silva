f6432be290d75bb09c14624c28b4757dd3166095
Daniel Melo de Andrade Silva <daniel.mandrade77@gmail.com>
2022-09-15 17:40:09 -0300

    create recipe

--- modulo5/cookenu/src/endpoints.ts/RecipeEndpoint.ts
+++ modulo5/cookenu/src/endpoints.ts/RecipeEndpoint.ts
@@ -1,18 +1,29 @@
 import { Request, Response } from "express";
 import { RecipeDatabase } from "../data/RecipeDatabase";
+import { InvalidToken } from "../error/InvalidToken";
 import { MissingFields } from "../error/MissingFields";
+import { GenerateId } from "../services/GenerateId";
 
 export class RecipeEndpoint {
 
     async createRecipe(req: Request, res: Response) {
         try {
+
+            const token = req.headers.authorization as string
             const {title, description} = req.body
 
             if(!title || !description) {
                 throw new MissingFields()
             }
+
+            if(!token) {
+                throw new InvalidToken()
+            }
             
-            const recipeDate = new RecipeDatabase()
+            const recipeData = new RecipeDatabase()
+            
+            const id = new GenerateId().createId()
+
             
         } catch (error: any) {
              res.status(error.statusCode || 500).send({message: error.message})
